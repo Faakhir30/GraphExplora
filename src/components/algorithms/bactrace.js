d9bd28e6da54c85644abb0296c8c283f19ecf50d
Faakhir30 <zahidfaakhir@gmail.com>
2023-07-13 00:42:14 +0500

    implemented backtrace

---
+++ src/components/algorithms/bactrace.js
@@ -0,0 +1,9 @@
+export function getNodesInPathOrder(finishNode) {
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
+    while (currentNode !== null) {
+      nodesInShortestPathOrder.unshift(currentNode);
+      currentNode = currentNode.previousNode;
+    }
+    return nodesInShortestPathOrder;
+  }
