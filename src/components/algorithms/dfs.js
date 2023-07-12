04e5930a41c9903efbb1c5c8bcec7023f4d553bd
Faakhir30 <zahidfaakhir@gmail.com>
2023-07-13 00:36:32 +0500

    added: dfs algorithm

---
+++ src/components/algorithms/dfs.js
@@ -0,0 +1,33 @@
+export function dfs(grid, startNode, finishNode) {
+  let frontier = [startNode];
+  let explored = [];
+  while (true) {
+    if (frontier.length === 0) return explored.reverse();
+    let node = frontier.shift();
+    explored.unshift(node);
+    for (const neighborNode of getUnvisitedNeighbors(node, grid)) {
+      if (neighborNode === finishNode) {
+        neighborNode.previousNode = node;
+        return explored.reverse();
+      }
+      if (
+        !explored.includes(neighborNode) &&
+        !frontier.includes(neighborNode) &&
+        !neighborNode.isWall
+      ) {
+        neighborNode.previousNode = node;
+        frontier.unshift(neighborNode);
+      }
+    }
+  }
+}
+
+function getUnvisitedNeighbors(node, grid) {
+  const neighbors = [];
+  const { col, row } = node;
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+  return neighbors.filter((neighbor) => !neighbor.isVisited);
+}
